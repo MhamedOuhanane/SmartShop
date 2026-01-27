840f71a1dc71b65382f6f558508b847327ae22b6
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-01-27 17:04:09 +0100

    SMAR-76 #done: Implémentation de l'Intercepteur de Réponse (Gestion 401/403)

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, {AxiosError} from 'axios'
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -6,6 +6,27 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
-})
+});
+
+api.interceptors.response.use(
+    response => response,
+    (error: AxiosError)=> {
+        if (!error.response) {
+            console.error("Network error:", error)
+            return Promise.reject(error)
+        }
+
+        const status: number = error.response?.status
+
+        if (status === 401) {
+            console.log("Unauthorized! Redirect to login")
+            window.location.href = "/login"
+        } else if (status === 403) {
+            console.log("Forbidden! Access denied")
+        }
+
+        return Promise.reject(error)
+    }
+);
 
 export default api;
