a639541e5d64e9e37a6d8e49178885ed4b12f1a0
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-01-27 16:44:33 +0100

    SMAR-74 #done: Instance Axios et Configuration de base Création d'un service API centralisé

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Provider} from "react-redux";
+import {store} from "./app/store.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root');
+if (!container) throw new Error('Root element not found')
+
+createRoot(container).render(
   <StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </StrictMode>,
 )
